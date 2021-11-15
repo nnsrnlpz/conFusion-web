2da0e255d010d3f3ea38cef9c18c7407ea0f074d
Anna <nnsrnlpz@gmail.com>
2021-11-15 14:43:01 +0100

    Template Driven forms part II

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,9 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 // Components
 import { AppComponent } from './app.component';
@@ -61,7 +64,10 @@ import { LoginComponent } from './login/login.component';
     MatFormFieldModule,
     MatInputModule,
     MatCheckboxModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatSlideToggleModule
   ],
   providers: [
     DishService,
---
+++ src/app/shared/feedback.ts
@@ -0,0 +1,14 @@
+// Data Model
+
+export class Feedback {
+    firstname!: string;
+    lastname!: string;
+    telnum!: number;
+    email!: string;
+    agree!: boolean;
+    contactype!: string;
+    message!: string;
+
+}
+
+export const ContactType = ['None', 'Tel', 'Email'];
